561b243a1dece7a7bf83a74a08ef89cc61190f87
PLJNS <me@pljns.com>
2024-02-21 10:59:39 -0500

    Add android target

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adeptry.nutritionplanner',
+  appName: 'Nutrition Planner',
+  webDir: 'release/app/dist/renderer',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
