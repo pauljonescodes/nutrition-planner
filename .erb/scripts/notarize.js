3372f95280976f6304188fc53be14c80d01ef020
Paul Jones <paul.jones@adeptry.com>
2024-02-22 13:17:36 -0500

    Add APPLE_TEAM_ID

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -28,5 +28,6 @@ exports.default = async function notarizeMacos(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
   });
 };
